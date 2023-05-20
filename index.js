2c0723db6599766a4749b19bcfebc70fc4db4e9a
Joshua Tafoya <tafoyajd.91@gmail.com>
2023-05-19 22:21:46 -0700

    created transport functions

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (pu) {
+    if (pu > 42) {
+        return pu - 42;
+    } else {
+        return 42 - pu;
+    }
+}
+
+function distanceFromHqInFeet (pu) {
+    if (pu > 42) {
+        return (pu - 42) * 264;
+    } else {
+        return (42 - pu) * 264;
+    }
+}
+
+function distanceTravelledInFeet (pu, fin) {
+    return Math.abs((pu - fin) * 264);
+}
+
+function calculatesFarePrice (pu, fin) {
+    let distance = Math.abs((pu - fin) * 264);
+    if (distance < 400) {
+        return 0;
+    } else if (distance < 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance < 2500) { 
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
